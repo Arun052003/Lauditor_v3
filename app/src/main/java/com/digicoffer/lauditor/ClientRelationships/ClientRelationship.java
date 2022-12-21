79a7d390a2e542862ec914087f8100f2409bd1f4
Rajendra <rajendra.sai@digicoffer.com>
2022-12-21 14:56:19 +0530

    1.Button functionalities added - client relationships

--- app/src/main/java/com/digicoffer/lauditor/ClientRelationships/ClientRelationship.java
+++ app/src/main/java/com/digicoffer/lauditor/ClientRelationships/ClientRelationship.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
@@ -17,8 +18,10 @@ import com.digicoffer.lauditor.Webservice.HttpResultDo;
 
 public class ClientRelationship extends Fragment implements AsyncTaskCompleteListener {
 
-    private RadioGroup rv_add_relationships;
-    RadioButton rb_add_relationship, rb_view_relationships;
+    private RadioGroup rg_add_relationships,rg_individual_entity;
+    RadioButton rb_add_relationship, rb_view_relationships,rb_individual,rb_entity;
+    LinearLayout ll_entity_name,ll_contact_person,ll_first_name,ll_last_name,ll_contatc_phone;
+
 
     @Override
     public void onClick(View view) {
@@ -35,12 +38,18 @@ public class ClientRelationship extends Fragment implements AsyncTaskCompleteLis
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        rv_add_relationships = view.findViewById(R.id.rgTask);
+        rg_add_relationships = view.findViewById(R.id.rgTask);
         rb_add_relationship = view.findViewById(R.id.add_relationship);
         rb_view_relationships = view.findViewById(R.id.view_relationship);
+        rg_individual_entity = view.findViewById(R.id.entity);
+        rb_individual = view.findViewById(R.id.add_individiual);
+        rb_entity = view.findViewById(R.id.add_entity);
+        ll_entity_name = view.findViewById(R.id.ll_entity_name);
+        ll_contact_person  = view.findViewById(R.id.ll_contact_person);
+        ll_contatc_phone = view.findViewById(R.id.ll_entity_number);
         rb_add_relationship.setTextColor(getContext().getResources().getColor(R.color.white));
         rb_view_relationships.setTextColor(getContext().getResources().getColor(R.color.black));
-        rv_add_relationships.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+        rg_add_relationships.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup radioGroup, int i) {
                 switch (i) {
@@ -61,7 +70,41 @@ public class ClientRelationship extends Fragment implements AsyncTaskCompleteLis
                 }
             }
         });
+        rg_individual_entity.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                switch (i) {
+                    case R.id.add_individiual:
+                        hideEntityData();
+                        rb_individual.setBackgroundDrawable(getContext().getResources().getDrawable(R.drawable.button_left_green_background));
+                        rb_entity.setTextColor(getContext().getResources().getColor(R.color.white));
+                        rb_entity.setBackgroundDrawable(getContext().getResources().getDrawable(R.drawable.button_right_background));
+                        rb_individual.setTextColor(getContext().getResources().getColor(R.color.white));
+                        rb_entity.setTextColor(getContext().getResources().getColor(R.color.black));
+                        break;
+                    case R.id.add_entity:
+                            unhideEntityData();
+                        rb_individual.setBackgroundDrawable(getContext().getResources().getDrawable(R.drawable.button_left_background));
+                        rb_entity.setBackgroundDrawable(getContext().getResources().getDrawable(R.drawable.button_right_green_count));
+                        rb_individual.setTextColor(getContext().getResources().getColor(R.color.black));
+                        rb_entity.setTextColor(getContext().getResources().getColor(R.color.white));
+                        break;
+                }
+            }
+        });
 
+    }
+
+    private void hideEntityData() {
+        ll_entity_name.setVisibility(View.GONE);
+        ll_contact_person.setVisibility(View.GONE);
+        ll_contatc_phone.setVisibility(View.GONE);
+    }
+
+    private void unhideEntityData() {
+        ll_entity_name.setVisibility(View.VISIBLE);
+        ll_contact_person.setVisibility(View.VISIBLE);
+        ll_contatc_phone.setVisibility(View.VISIBLE);
     }
 
     @Override

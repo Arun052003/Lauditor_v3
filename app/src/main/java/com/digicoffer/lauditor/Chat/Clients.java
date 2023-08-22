008e50efb3c8965a24385bd7026c94145c900c67
Kumar <kumar.r@digicoffer.com>
2023-08-22 13:18:48 +0530

    check the development branch changes.

--- app/src/main/java/com/digicoffer/lauditor/Chat/Clients.java
+++ app/src/main/java/com/digicoffer/lauditor/Chat/Clients.java
@@ -42,6 +42,7 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
 
+//Client Chat
 public class Clients extends Fragment implements AsyncTaskCompleteListener, ChatAdapter.EventListener {
     AlertDialog progress_dialog;
     RecyclerView rv_Clientrelationships;
